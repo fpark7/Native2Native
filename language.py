115a90695e6e7fa61c076f515cf35d2f0312df20
Felix Park <felix.park03@gmail.com>
2018-03-25 00:47:45 -0400

    help me

--- language.py
+++ language.py
@@ -10,7 +10,13 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch import optim
 import torch.nn.functional as F
-USE_CUDA = False
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import numpy as np
+%matplotlib inline
+
+USE_CUDA = True
 SOS_token = 0
 EOS_token = 1
 
@@ -295,3 +301,209 @@ input_lang, output_lang, pairs = prepare_data('eng', 'fra', True)
 # Print an example pair
 print(random.choice(pairs))
 testStuff()
+
+teacher_forcing_ratio = 0.5
+clip = 5.0
+
+def train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+
+    # Zero gradients of both optimizers
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    loss = 0 # Added onto for each word
+
+    # Get size of input and target sentences
+    input_length = input_variable.size()[0]
+    target_length = target_variable.size()[0]
+
+    # Run words through encoder
+    encoder_hidden = encoder.init_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+    
+    # Prepare input and output variables
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))
+    decoder_context = Variable(torch.zeros(1, decoder.hidden_size))
+    decoder_hidden = encoder_hidden # Use last hidden state from encoder to start decoder
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+        decoder_context = decoder_context.cuda()
+
+    # Choose whether to use teacher forcing
+    use_teacher_forcing = random.random() < teacher_forcing_ratio
+    if use_teacher_forcing:
+        
+        # Teacher forcing: Use the ground-truth target as the next input
+        for di in range(target_length):
+            decoder_output, decoder_context, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_context, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output[0], target_variable[di])
+            decoder_input = target_variable[di] # Next target is next input
+
+    else:
+        # Without teacher forcing: use network's own prediction as the next input
+        for di in range(target_length):
+            decoder_output, decoder_context, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_context, decoder_hidden, encoder_outputs)
+            loss += criterion(decoder_output[0], target_variable[di])
+            
+            # Get most likely word index (highest value) from output
+            topv, topi = decoder_output.data.topk(1)
+            ni = topi[0][0]
+            
+            decoder_input = Variable(torch.LongTensor([[ni]])) # Chosen word is next input
+            if USE_CUDA: decoder_input = decoder_input.cuda()
+
+            # Stop at end of sentence (not necessary when using known targets)
+            if ni == EOS_token: break
+
+    # Backpropagation
+    loss.backward()
+    torch.nn.utils.clip_grad_norm(encoder.parameters(), clip)
+    torch.nn.utils.clip_grad_norm(decoder.parameters(), clip)
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+    
+    return loss.data[0] / target_length
+
+def as_minutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def time_since(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (as_minutes(s), as_minutes(rs))
+
+attn_model = 'general'
+hidden_size = 500
+n_layers = 2
+dropout_p = 0.05
+
+# Initialize models
+encoder = EncoderRNN(input_lang.n_words, hidden_size, n_layers)
+decoder = AttnDecoderRNN(attn_model, hidden_size, output_lang.n_words, n_layers, dropout_p=dropout_p)
+
+# Move models to GPU
+if USE_CUDA:
+    encoder.cuda()
+    decoder.cuda()
+
+# Initialize optimizers and criterion
+learning_rate = 0.0001
+encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
+decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
+criterion = nn.NLLLoss()
+
+
+##variables
+# Configuring training
+n_epochs = 50000
+plot_every = 200
+print_every = 1000
+
+# Keep track of time elapsed and running averages
+start = time.time()
+plot_losses = []
+print_loss_total = 0 # Reset every print_every
+plot_loss_total = 0 # Reset every plot_every
+
+
+# Begin!
+for epoch in range(1, n_epochs + 1):
+    
+    # Get training data for this cycle
+    training_pair = variables_from_pair(random.choice(pairs))
+    input_variable = training_pair[0]
+    target_variable = training_pair[1]
+
+    # Run the train function
+    loss = train(input_variable, target_variable, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion)
+
+    # Keep track of loss
+    print_loss_total += loss
+    plot_loss_total += loss
+
+    if epoch == 0: continue
+
+    if epoch % print_every == 0:
+        print_loss_avg = print_loss_total / print_every
+        print_loss_total = 0
+        print_summary = '%s (%d %d%%) %.4f' % (time_since(start, epoch / n_epochs), epoch, epoch / n_epochs * 100, print_loss_avg)
+        print(print_summary)
+
+    if epoch % plot_every == 0:
+        plot_loss_avg = plot_loss_total / plot_every
+        plot_losses.append(plot_loss_avg)
+        plot_loss_total = 0
+def show_plot(points):
+    plt.figure()
+    fig, ax = plt.subplots()
+    loc = ticker.MultipleLocator(base=0.2) # put ticks at regular intervals
+    ax.yaxis.set_major_locator(loc)
+    plt.plot(points)
+
+show_plot(plot_losses)
+
+def evaluate(sentence, max_length=MAX_LENGTH):
+    input_variable = variable_from_sentence(input_lang, sentence)
+    input_length = input_variable.size()[0]
+    
+    # Run through encoder
+    encoder_hidden = encoder.init_hidden()
+    encoder_outputs, encoder_hidden = encoder(input_variable, encoder_hidden)
+
+    # Create starting vectors for decoder
+    decoder_input = Variable(torch.LongTensor([[SOS_token]])) # SOS
+    decoder_context = Variable(torch.zeros(1, decoder.hidden_size))
+    if USE_CUDA:
+        decoder_input = decoder_input.cuda()
+        decoder_context = decoder_context.cuda()
+
+    decoder_hidden = encoder_hidden
+    
+    decoded_words = []
+    decoder_attentions = torch.zeros(max_length, max_length)
+    
+    # Run through decoder
+    for di in range(max_length):
+        decoder_output, decoder_context, decoder_hidden, decoder_attention = decoder(decoder_input, decoder_context, decoder_hidden, encoder_outputs)
+        decoder_attentions[di,:decoder_attention.size(2)] += decoder_attention.squeeze(0).squeeze(0).cpu().data
+
+        # Choose top word from output
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        if ni == EOS_token:
+            decoded_words.append('<EOS>')
+            break
+        else:
+            decoded_words.append(output_lang.index2word[ni])
+            
+        # Next input is chosen word
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        if USE_CUDA: decoder_input = decoder_input.cuda()
+    
+    return decoded_words, decoder_attentions[:di+1, :len(encoder_outputs)]
+
+def evaluate_randomly():
+    pair = random.choice(pairs)
+    
+    output_words, decoder_attn = evaluate(pair[0])
+    output_sentence = ' '.join(output_words)
+    
+    print('>', pair[0])
+    print('=', pair[1])
+    print('<', output_sentence)
+    print('')
+
+
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
+evaluate_randomly()
