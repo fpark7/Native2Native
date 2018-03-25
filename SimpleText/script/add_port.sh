81f281d6eb4ee14af0eff3794dc93cab0ab34633
FengyangZhang <zhfy888@163.com>
2018-03-25 05:14:25 -0400

    update script to run the model

--- SimpleText/script/add_port.sh
+++
@@ -1,3 +0,0 @@
-sudo iptables -A INPUT -p tcp --dport 8008 -m state --state NEW -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 6006 -m state --state NEW -j ACCEPT
-sudo iptables -A INPUT -p tcp --dport 8888 -m state --state NEW -j ACCEPT
--- SimpleText/script/test_overfit.sh
+++
@@ -1,4 +0,0 @@
-python -m nmt.nmt.nmt\
-	--out_dir=model/overfit_attention_glove\
-	--inference_input_file=data/PWKP/val.complex\
-	--inference_output_file=data/PWKP/val.overfit
